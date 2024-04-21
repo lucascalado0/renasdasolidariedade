dece19e4b29158fa11632abbe4aff07ea6e68284
devlucascalado <devlucascalado@gmail.com>
2024-04-21 13:52:33 -0300

    corrigindo localização

--- assets/js/whatWeb.js
+++ assets/js/whatWeb.js
@@ -1,9 +1,14 @@
 alert('Bem vindo, ative a localização do seu aparelho!')
 
-
 var target = document.getElementById('target');
 var watchId;
 
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
 
 if ('geolocation' in navigator) {
   document.getElementById('askButton').addEventListener('click', function () {
@@ -14,22 +19,4 @@ if ('geolocation' in navigator) {
   });
 } else {
   target.innerText = 'Geolocation API not supported.';
-}
-
-
-const url = 'https://map-geocoding.p.rapidapi.com/json?latlng=40.714224%2C-73.961452';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '164206ff9fmsh2954e1ca7309153p16a87djsnd521d42505bc',
-		'X-RapidAPI-Host': 'map-geocoding.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+ }
